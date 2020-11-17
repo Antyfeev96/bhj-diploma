216536bab48f11b39ccaf59571027b3051b8884c
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2020-11-17 15:47:07 +0300

    Any commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,6 +3,15 @@
  * на сервер.
  * */
 function createRequest(options) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open( options.method, options.url );
+    xhr.withCredentials = true;
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            options.callback(xhr.response.error, xhr.response)
+        }
+    })
     
         try {
 
@@ -22,17 +31,15 @@ function createRequest(options) {
         }
         catch ( e ) {
             // перехват сетевой ошибки
-            options.callback(e, response);
+            options.callback(e, xhr.response);
         }
-     
 
-    const xhr = new XMLHttpRequest();
-    xhr.open( options.method, options.url );
-    xhr.withCredentials = true;
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.status === 200 && xhr.readyState === 4) {
-            options.callback(err, response)
-        }
-    })
+        // Оставил в таком виде, т.к. будет странно, если я оставлю в try/catch только xhr.send(),
+        // Во-первых, отправки разные и мне не избежать повтора if/else. Сначала if/else будет вне блока
+        // try/catch, чтобы задать option.url или formData, а потом будет if/else в блоке try/catch 
+        // с точно таким же условием, потому что в одном условии отправляется xhr.send(), а в другом 
+        // xhr.send(formData). Конечно можно создать формдату и просто потом по условию не использовать её,
+        // но я не знаю, нормальная ли такая практика или нет, но даже в таком случае присваивание options.url останется
+        // в блоке try/catch
 
 };
